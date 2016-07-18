bf0e7e6326750dcd5128dd5b237056e6e3b8100b
AvengedEndie <prakash.nitin63@gmail.com>
2016-07-18 21:06:59 +0530

    maximum element in an array

---
+++ Arrays/maximum_element.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int n, i, hi;
+	
+	printf("Enter the number of elements in the array> ");
+	scanf("%d", &n);
+
+	int a[n];
+	
+	for(i = 0; i < n; i++) {
+		printf("Enter element> ");
+		scanf("%d", &a[i]);
+	}
+
+	hi = a[0];
+
+	for(i = 0; i < n; i++) {
+		if(hi < a[i])
+		hi = a[i];
+	}
+
+	printf("\n%d is the maximum element in the array\n", hi);
+  
+return 0;
+}
