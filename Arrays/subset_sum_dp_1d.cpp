c3f75693331aeaa3a823a501d03b93bc21101aa7
jaskamalkainth <kainthjaskamal@gmail.com>
2016-10-02 19:27:35 +0530

    Added Explanation for Susbet Sum Dynamic Programming

--- Arrays/subset_sum_dp_1d.cpp
+++ Arrays/subset_sum_dp_1d.cpp
@@ -1,4 +1,3 @@
-// 1d saves memory O(sum).
 
 /*
  * Dynamic Programming | (Subset Sum Problem)
@@ -12,23 +11,28 @@
 using namespace std;
 
 
+// 1d saves memory O(sum) = Space Complexity 
 bool is_subset_sum (vector<int>& v,int sum)
 {
 	int n=v.size();
 
 	vector<int> dp(sum+1,0);
 
-	dp[0]=1; //sum =0 is always attainable.
+	dp[0]=1;
 
+	//sum  =  0 is always attainable.
 	for(int i = 0; i<n; i++)
 	{
 		for(int j = sum; j >= v[i]; j--)
 		{
 			dp[j] |= dp[j-v[i]];
+			// either include v[i] or do not include v[i].
+			// if you include v[i] then then look if we can make ( j - v[i] ).
+			// These are the two Sub-problems.
 		}
 	}
-
 	return dp[sum];
+
 	//dp[i] = 1 if we can make sum = i
 	// if sum of all array ele < desired sum then 0. 
 }
@@ -40,8 +44,10 @@ int main()
 	vector<int> input(n);
 	for(int i = 0; i < n; i++)
 		cin >> input[i];
+	// Array of n numbers
 	int num_to_check; 
 	cin >> num_to_check;
+	// Actual Sum value you want to check.
 	cout << is_subset_sum(input, num_to_check) << "\n";
 
 	return 0;
