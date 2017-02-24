cc578048fb356e8c3f10a530495259aff3b870f2
Nitin Prakash <prakash.nitin63@gmail.com>
2017-02-24 15:57:55 +0530

    add functions and comments to heapsort

--- Sorting/heapsort.c
+++ Sorting/heapsort.c
@@ -1,61 +1,63 @@
-#include<stdio.h>
+#include <iostream>
 
-int main()
-{
-	int size, i, j, c, root;
-	printf("Enter size of elements : ");
-	scanf("%d", &size);
-	int heap[size];
-	printf("Enter the values of the array : ");
-	for (i = 0;i < size;i++)
-	{
-		scanf("%d", &heap[i]);
+
+using namespace std;
+ 
+// To heapify a subtree rooted with node i which is
+void heapify(int arr[], int n, int i) {		// n is size of heap
+
+	int largest = i;  // Initialize largest as root
+	int l = 2 * i + 1;  // left = 2*i + 1
+	int r = 2 * i + 2;  // right = 2*i + 2
+ 
+	// If left child is larger than root
+	if (l < n && arr[l] > arr[largest])
+		largest = l;
+
+	// If right child is larger than largest so far
+	if (r < n && arr[r] > arr[largest])
+		largest = r;
+
+	// If largest is not root
+	if (largest != i) {
+		swap(arr[i], arr[largest]);
+ 
+		// Recursively heapify the affected sub-tree
+		heapify(arr, n, largest);
 	}
-	int temp;
-	for (i = 1;i < size;i++)
-	{
-		c = i;
-		do
-		{
-			root = (c - 1) / 2;             
-			if(heap[root] < heap[c])
-			{
-				temp = heap[root];
-				heap[root] = heap[c];
-				heap[c] = temp;
-			}
-			c = root;
-		} 
-		while(c != 0);
+}
+ 
+// main function to do heap sort
+void heapSort(int arr[], int n) {
+	// Build heap (rearrange array)
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapify(arr, n, i);
+ 
+	// One by one extract an element from heap
+	for (int i = n - 1; i >= 0; i--) {
+		// Move current root to end
+		swap(arr[0], arr[i]);
+ 
+		// call max heapify on the reduced heap
+		heapify(arr, i, 0);
 	}
-	for (j = size - 1; j >= 0; j--)
-	{
-		temp = heap[0];
-		heap[0] = heap[j];
-		heap[j] = temp;
-		root = 0;
-		do
-		{
-			c = 2 * root + 1;
-			if ((heap[c] < heap[c + 1]) && c < j-1)
-			{
-				c++;
-			}
-			if (heap[root]<heap[c] && c<j)
-			{
-				temp = heap[root];
-				heap[root] = heap[c];
-				heap[c] = temp;
-			}
-			root = c;
-		}
-		while (c < j);
-	} 
-	printf("The sorted array is : ");
-	for (i = 0; i < size; i++)
-	{
-		printf("%d ", heap[i]);
-	}
-	printf("\n");
-	return 0;
+}
+
+/* A utility function to print array of size n */
+void printArray(int arr[], int n) {
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	
+	cout << "\n";
+}
+ 
+// Driver program
+int main() {
+	int arr[] = {12, 11, 13, 5, 6, 7};
+	int n = sizeof(arr)/sizeof(arr[0]);
+ 
+	heapSort(arr, n);
+ 
+	cout << "Sorted array is \n";
+	printArray(arr, n);
 }
